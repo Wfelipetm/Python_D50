2dbdb918d4b4311d12e30c5939dbb44628cdb7c0
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-09-23 11:06:27 -0300

    Update 010.py

--- 010.py
+++ 010.py
@@ -1,3 +1,12 @@
+'''Faça um programa que leia dois números e imprima:
+a) A soma dos números.
+b) O produto do promeiro número pelo quadrado do segundo;
+c) O quadrado do primeiro número;
+d) A raiz quadrada da soma dos quadrados.
+e) O seno da diferença do primeiro número pelo segundo.'''
+
+
+
 import math
 
 n1 = float(input('Digite primeiro valor: '))
