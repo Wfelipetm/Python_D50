c2aa73d8c560da003bbc381ff9a8cc5cd89f71d2
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-09-26 14:28:35 -0300

    Update 014.py

--- 014.py
+++ 014.py
@@ -1,4 +1,4 @@
-# Faça um programa que leia uma string e imprima a), b), c), d).
+# Faça um programa que leia uma string e imprima:
 # a) Quantidade caracteres tem a string.
 # b) Quantidade caracteres são de pontuação....')
 # c) Quantidade caracteres são de número.
