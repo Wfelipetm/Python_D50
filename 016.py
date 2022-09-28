5a28c4b57412c698160e1ebe4b195f8d9bdb488e
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-09-27 23:40:12 -0300

    Update 016.py

--- 016.py
+++ 016.py
@@ -3,7 +3,7 @@
     vencimento, o pagamento deve ser acrescido de um valor de multa e
     um juros proporcional ao número de dias do atraso.
 
-    *Considerando a multa como 3% do valor do aluguel eo juros simples 
+    *Considerando a multa como 3% do valor do aluguel e o juros simples 
     de 0.05% do valor do aluguel por dia de atraso.
 
     *Faça um programa que receba o valor do aluguel e a quantidade de dias de atraso. 
@@ -14,13 +14,13 @@ while True:
     v_aluguel = float(input("Digite o valor do aluguel: R$ "))
     dias_atraso = float(input("Quantos dias de atraso: "))
 
-    def multa_atraso():
+    def multa_aluguel():
         return 0.03 * v_aluguel
 
-    def multa_por_dias():
+    def multa_por_dia():
         return dias_atraso * 0.005 * v_aluguel
 
-    valor_a_pagar = v_aluguel + multa_atraso() + multa_por_dias()
+    valor_a_pagar = v_aluguel + multa_aluguel() + multa_por_dia()
     print('Valor a pagar com juros e multa {:.2f}'.format(valor_a_pagar))
 
     sair = input('Deseja calcular juros e multa novamante? [s]im ou [n]ão: ')
