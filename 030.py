e0e7c0b351ea5720df9abbc734e8ef679026a470
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-04 14:50:34 -0300

    Update 030.py

--- 030.py
+++ 030.py
@@ -1,3 +1,9 @@
+'''Vamos fazer o sistema inverso do desafio 29.
+Leia duas strings, uma será a string censurada e outra com as vogais que foram removidas.
+Substitua os asteriscos pelas vogais e encontre o texto original.'''
+
+
+
 def LerTextoUsuario(mensagem):
     texto = input(mensagem)
     return texto
