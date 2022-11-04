27b0aff5896c616570da15d34a034bbe4c10067a
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-04 14:51:43 -0300

    Update 031.py

--- 031.py
+++ 031.py
@@ -1,3 +1,9 @@
+'''Vamos fazer um codificador de string simples.
+Leia um texto digitado pelo usuário e desloque cada caractere 3 posições para a direita.
+No final imprima a string codificada.'''
+
+
+
 def LerTextoUsuario(mensagem):
     texto = input(mensagem)
     return texto
