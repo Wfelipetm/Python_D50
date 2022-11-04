4f155bd4d0e7cd2c17b0ef2c2a1fd860096c1030
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-04 15:04:51 -0300

    Update 043.py

--- 043.py
+++ 043.py
@@ -1,3 +1,9 @@
+'''Vamos criar um sistema para contabilizar votos.
+Para este sistema temos 4 candidatos, cada um representado por um número.
+O usuário deve digitar o voto e confirmar, a partir daí o voto é registrado para o candidato específico, voto em branco ou voto nulo.
+No final, quando o usuário digitar zero, o sistema deve parar e imprimir as informações registradas na tela.'''
+
+
 import os
 
 votacao = {19: {'nome': 'Leonardo',
