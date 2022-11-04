56879251affbc736666f37ddd0e2601ebe021ab6
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-04 15:03:41 -0300

    Update 041.py

--- 041.py
+++ 041.py
@@ -1,5 +1,8 @@
-'''Vamos fazer um sistema para uma empresa conseguir controlar o quanto de armazenamento no servidor cada funcionário está utilizando. O departamento de TI gerou um relatório “usuarios.txt” contendo o nome do usuário e quantos bytes eles têm armazenado no servidor.
-Gere um relatório “relatorio.txt” contendo o espaço utilizado por usuário em MB, juntamente com a porcentagem de uso em relação ao total.
+'''Vamos fazer um sistema para uma empresa conseguir controlar o quanto de armazenamento
+no servidor cada funcionário está utilizando. O departamento de TI gerou um 
+relatório “usuarios.txt” contendo o nome do usuário e quantos bytes eles têm armazenado no servidor.
+Gere um relatório “relatorio.txt” contendo o espaço utilizado por usuário em MB, 
+juntamente com a porcentagem de uso em relação ao total.
 No final do relatório coloque o somatório total de espaço armazenado e qual a média por usuário.'''
 
 
