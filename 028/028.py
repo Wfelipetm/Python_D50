7ac715aa4d75e1f6ddebdcb309890eb219954c6c
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:24:30 -0300

    Update 028.py

--- 028/028.py
+++ 028/028.py
@@ -1,10 +1,3 @@
-'''Vamos fazer um programa que leia um texto digitado pelo usuário e imprima na tela se o texto digitado é palíndromo ou não!
-Para um texto ser palíndromo, ele pode ser lido da mesma maneira nos dois sentidos: da frente para trás e de trás para frente.
-Exemplos de palavras palíndromas: Anna, Otto, Reviver, Osso.
-Mas também podem ser frases como: “Socorram-me, subi no ônibus em Marrocos'''
-
-
-
 import unicodedata
 
 
