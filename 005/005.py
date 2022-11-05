b7ddd773dff890eb27b4f57980172938ec7c6168
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:04:47 -0300

    Update 005.py

--- 005/005.py
+++ 005/005.py
@@ -1,8 +1,3 @@
-'''Faça um programa que receba um número
- inteiro positivo de 1 até N.Imprima na 
- tela o resultado da soma.'''
-
-
 n = -1
 while (n <= 0):
     n = int(input('Digite um numero positivo: '))
