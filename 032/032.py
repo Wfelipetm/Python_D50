31a44e8d9c1e2d8cf96fc401ce336c216c02bed7
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:26:59 -0300

    Update 032.py

--- 032/032.py
+++ 032/032.py
@@ -1,12 +1,3 @@
-'''Vamos fazer um decodificador simples de strings.
-Agora vamos fazer o processo inverso do desafio 31. Lendo uma string já codificada pelo usuário 
-(utilizando o algoritmo do desafio 31) você irá fazer o processo inverso para decodificá-la, 
-subtraindo 3 de cada letra e descobrindo o texto original.'''
-
-
-
-No final imprima o texto decodificado na tela.
-
 def LerTextoUsuario(mensagem):
     texto = input(mensagem)
     return texto
