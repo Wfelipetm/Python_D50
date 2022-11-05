31c4422ee424ed59d39285e9b098cc24e1f8d502
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:12:18 -0300

    Update 014.py

--- 014/014.py
+++ 014/014.py
@@ -1,9 +1,3 @@
-# Faça um programa que leia uma string e imprima:
-# a) Quantidade caracteres tem a string.
-# b) Quantidade caracteres são de pontuação....')
-# c) Quantidade caracteres são de número.
-# d) Quantidade caracteres quantos são vogais.
-
 frase = input('Digite uma frase: ')
 print()
 
