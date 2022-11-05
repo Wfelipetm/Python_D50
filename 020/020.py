7248ce7a8534d4f0c0b738bed272124597ae5ffa
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:18:09 -0300

    Update 020.py

--- 020/020.py
+++ 020/020.py
@@ -1,10 +1,3 @@
-'''Vamos fazer um programa que fique lendo uma sequência de palavras digitadas pelo usuário, uma a uma,
-   até que o usuário diga que finalizou a entrada de palavras.
-   Armazene as palavras digitadas pelo usuário em uma lista.
-   Após o usuário finalizar a digitação, percorra a lista e procure pela maior palavra.
-   Se mais de uma palavra empatar em tamanho, imprima todas elas!'''
-
-
 def lerPalavra(palavras):
     palavra = input("\n Digite uma palavra: ")
     palavras.append(palavra)
