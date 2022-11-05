104d338b054ca4c0ffe557933d4a0ee520f78e30
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:15:35 -0300

    Update 017.py

--- 017/017.py
+++ 017/017.py
@@ -1,12 +1,3 @@
-'''Neste desafio vamos ler os dados do funcionário e calcular o salário bruto, os descontos de INSS e Imposto de renda.
-No final vamos imprimir os dados do funcionário.
-a. A empresa paga R$23,00 reais por hora.
-b. A empresa paga um adcional de R$ 34,00 por dependente.
-c. É descontado um valor de 8.5% para o INSS e 15% de IR.'''
-
-
-
-
 print("--------------------------------------------")
 
 
