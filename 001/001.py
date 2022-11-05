585735107e34226817dc124705ef17fec3c5de02
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:39:04 -0300

    Update 001.py

--- 001/001.py
+++ 001/001.py
@@ -1,6 +1,3 @@
-'''Faça um programa que entre o preço de um produto e o valor em dinheiro pago.
-imprima para o usuário o quanto será o troco.'''
-
 Produto = int(input('Valor do produto: '))
 valorProduto = Produto
 Dinheiro = int(input('Valor do Cliente: '))
