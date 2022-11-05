3220670105586192297564d5c2b3dd328c334959
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:45:15 -0300

    Update 047.py

--- 047/047.py
+++ 047/047.py
@@ -1,12 +1,3 @@
-'''Vamos incrementar o desafio 39.
-Antes de solicitar o pedido, faça o programa ler o nome do usuário que está fazendo o pedido.
-Ao finalizar o pedido, armazene o pedido juntamente com o nome do cliente e a data e hora do sistema.
-Adicione uma opção para sair do sistema de pedidos.
-Quando isso for feito, imprima na tela os pedidos feitos no dia.'''
-
-
-
-
 import os
 from datetime import datetime
 
