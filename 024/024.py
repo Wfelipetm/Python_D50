84af7e10613d9eeeaf7b40a0cd551cb49439a838
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:21:35 -0300

    Update 024.py

--- 024/024.py
+++ 024/024.py
@@ -1,9 +1,3 @@
-'''Vamos ajudar as crianças a praticar a tabuada.
-Crie um programa que gere perguntas entre a tabuada do 1 e do 10, 
-e leia o resultado da pergunta. Caso o usuário acerte, você continua fazendo perguntas. 
-Quando o usuário errar, você informa a ele na tela e pergunta se ele quer tentar novamente.
-'''
-
 import random
 
 
