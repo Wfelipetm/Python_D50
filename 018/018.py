621a518ed17332bfee67126cf61e7400acca4477
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:16:25 -0300

    Update 018.py

--- 018/018.py
+++ 018/018.py
@@ -1,9 +1,3 @@
-'''Vamos fazer um programa que pensa um número entre 0 e 49.
-Após ele fica perguntando para o usuário tentar adivinhar. 
-Se o usuário digitar um valor menor ele avisa o usuário para tentar um valor maior e vice-versa.'''
-
-
-
 from faker import Faker
 
 falso = Faker('pt_BR')
