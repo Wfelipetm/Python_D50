f4861c529b381f76301e6f37fd39e3ccd4a4c9de
Wfelipetm <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:30:43 -0300

    Create 011.py

---
+++ 011/011.py
@@ -0,0 +1,42 @@
+'''Vamos fazer uma calculadora!
+Faça um programa que leia um valor, um operador (+, -, *, /) e um outro valor.
+Efetue a operação passada pelo usuário e imprima o resultado.'''
+
+
+
+while True:
+    print()
+    num_1 = input('digite um número:')
+    operadores = input('Digite um operador: ')
+    num_2 = input('Digite um número: ')
+
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Você precisa digitar um número. ')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)
+
+    # + - / *
+
+    if operadores == '+':
+        print(num_1 + num_2)
+
+    elif operadores == '-':
+        print(num_1 - num_2)
+
+    elif operadores == '/':
+        print(num_1 / num_2)
+
+    elif operadores == '*':
+        print(num_1 * num_2)
+
+    elif operadores == '**':
+        print(num_1 ** num_2)
+
+    else:
+        print('Operador invalido')
+    sair = input('Deseja sair? [s]im ou [n]ão: ')
+
+    if sair == 's':
+        break
