9c3eb8e535723abcabf94903024a671eb037d455
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:49:31 -0300

    Update 042.py

--- 042/042.py
+++ 042/042.py
@@ -1,10 +1,3 @@
-'''Vamos incrementar o desafio anterior adicionando um menu no nosso sistema.
-No menu você vai dar a opção para o usuário escolher o tipo de relatório que ele quer criar.
-Por padrão o relatório será gerado na mesma ordem de leitura do arquivo ‘usuarios.txt’, conforme fizemos no desafio anterior.
-Adicionaremos uma segunda opção onde o relatório será gerado com os usuários ordenados em ordem de utilização de armazenamento.
-Leia também o nome do arquivo de relatório a ser gerado (por padrão o sistema gera “relatório.txt”).'''
-
-
 import os
 
 MEGABYTE = 1024*1024
