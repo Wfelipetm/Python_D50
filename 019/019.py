e08153b4459bdc8b21360838a581a3966982d101
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:17:33 -0300

    Update 019.py

--- 019/019.py
+++ 019/019.py
@@ -1,9 +1,3 @@
-'''Vamos fazer uma função que verifica se um número é primo.
-Depois faça o programa ler um número inteiro positivo do usuário, 
-chame a função passando o valor como parâmetro e imprima o resultado, se o número que o usuário digitou é ou não primo.'''
-
-
-
 while True:
     def leia_valor_positivo(mensagem):
         valor = -1
