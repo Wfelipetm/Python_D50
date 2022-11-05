2b198418fdf38548bbddd557a5e58d02ace58372
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:13:24 -0300

    Update 015.py

--- 015/015.py
+++ 015/015.py
@@ -1,8 +1,3 @@
-'''Faça um programa que leia uma string e um caractere, removendo todas as 
-ocorrências este caractere lido da string inicialmente lida.
-imprima o resultado da string após a remoção dos caracteres.'''
-
-
 while True:
     print('--------------------------------------------------------')
 
