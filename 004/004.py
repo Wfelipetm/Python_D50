f6018108f7d828e3291c6759baa8261882985d9c
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:03:56 -0300

    Update 004.py

--- 004/004.py
+++ 004/004.py
@@ -1,9 +1,3 @@
-'''Faça um programa que receba duas dimenões de um 
-terreno e imprima a área deste terreno.considerando 
-que o preço do metro quadrado é de 7.592,00 , imprima 
-também o valor que custará este terreno'''
-
-
 frente = float(input('Qual a medida da frente do terreno? '))
 fundos = float(input('Qual o fundos do terreno? '))
 valorm2 = 7592.00
