07280942eba04abf485012405e48ed9645e71ccc
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:23:42 -0300

    Update 027.py

--- 027/027.py
+++ 027/027.py
@@ -1,11 +1,3 @@
-'''Vamos fazer um programa para encontrar o elemento faltante em uma lista.
-O seu programa irá novamente ler uma lista de inteiros de um arquivo texto.
-Para simplificar vamos considerar uma lista de elementos não ordenados com 49 elementos, 
-contendo números entre 1 e 50 sem repetir. Isso significa que um número dentro dessa sequência estará faltando.
-Faça uma função que irá receber a lista lida e retornará qual o inteiro que está faltando.'''
-
-
-
 def LePalavrasDoArquivo():
     palavras = []
     input_file = open('input.txt', 'r')
