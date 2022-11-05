ae86f20d06a4f676e9eceaf22008aae74076c88a
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:47:43 -0300

    Update 044.py

--- 044/044.py
+++ 044/044.py
@@ -1,12 +1,3 @@
-'''Vamos calcular o salário de um funcionário.
-Este funcionário foi contratado em 1995.
-No primeiro ano de trabalho, ele recebeu um aumento de 1.5% sobre o salário inicial.
-A partir dos próximos anos, a porcentagem de aumento progride em 10% anualmente (1.5%, 1.65%, 1.815%, 1.9965%, 2.196%, ...).
-Faça um programa que o usuário digite o salário inicial e o ano que deseja saber o salário.
-O programa deve ir calculando os aumentos até chegar no ano desejado e imprimir o salário calculado na tela.'''
-
-
-
 salarioInicial = float(input('Digite o salario inicial: R$ '))
 anoContratacao = int(input('Digite o ano de contratacao: '))
 anoFinal = int(input('Digite o ano que voce quer saber o salario: '))
