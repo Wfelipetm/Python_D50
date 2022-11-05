d64369465f74f73ec9e8a3478ab33331d4157ab3
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:42:41 -0300

    Update 049.py

--- 049/049.py
+++ 049/049.py
@@ -1,10 +1,3 @@
-'''Seguimos no sistema da lanchonete.
-Agora vamos criar uma opção no menu principal para que o proprietário consiga cadastrar novos produtos.
-Vamos deixar de escrever os produtos no código e vamos ler eles de um arquivo texto 'produtos.json'.
-Caso o proprietário escolha a opção de adicionar novos produtos, ao final do cadastramento, atualize o arquivo 'produtos.json'.'''
-
-
-
 import json
 import os
 from datetime import datetime
