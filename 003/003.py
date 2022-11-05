0d07e61858e4f03ca86fe717e9e94e63a341cb58
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:52:59 -0300

    Update 003.py

--- 003/003.py
+++ 003/003.py
@@ -1,8 +1,3 @@
-'''Faça um programa que receba 4 notas de um aluno,
- uma de cada bimestre, e calcule a média final.
- imprima a média final e se o aluno foi APROVADO ou REPROVADO,
-considerando que a média de aprovação seja igual ou superior a seis.'''
-
 notab1 = float(input('Nota primeiro Bimestre: '))
 notab2 = float(input('Nota segundo Bimestre: '))
 notab3 = float(input('Nota terceiro Bimestre: '))
