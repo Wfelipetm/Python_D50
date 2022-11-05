a5d44f3b022dd8b6881bf873cf244ed434cebc91
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:18:43 -0300

    Update 021.py

--- 021/021.py
+++ 021/021.py
@@ -1,10 +1,3 @@
-'''Vamos continuar o código do desafio 20!
-Para este desafio vamos criar uma função que lê a sequência de palavras de um arquivo texto chamado input.txt e 
-armazena essas palavras lidas em uma lista (a mesma utilizada no desafio anterior).
-Após feita esta leitura, faça a mesma coisa que no desafio 20.
-No final, escreva a (ou as) palavra com o maior tamanho em um outro arquivo chamado output.txt'''
-
-
 def lerPalavrasDoArquivo(palavras):
     input_file = open("input.txt", "r")
     conteudo = input_file.readlines()
