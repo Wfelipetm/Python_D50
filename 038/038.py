5daeb0d383137f8d214d128e4f9a454f9f943a32
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:31:10 -0300

    Update 038.py

--- 038/038.py
+++ 038/038.py
@@ -1,16 +1,3 @@
-'''Vamos fazer uma função que receba um ano por parâmetro 
-e retorne se este ano é bissexto ou não.
-Para determinar se um ano é bissexto você deve verificar:
-Se o ano é divisível por 4 e não é divisível por 100 OU
-Se o ano é divisível por 4, por 100 e por 400
-
-Faça um programa que leia um ano, utilize a função criada e 
-imprima o resultado na tela para o usuário, dizendo se o ano é bissexto ou não.'''
-
-
-
-
-
 def DesejaContinuar():
     continuar = input('Deseja continuar? [s]im/[n]ao: ')
     return continuar == "s"
