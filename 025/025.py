790bab98098d1288a9d4b8b14b9edad55eb609b1
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:22:07 -0300

    Update 025.py

--- 025/025.py
+++ 025/025.py
@@ -1,8 +1,3 @@
-'''Neste desafio vamos aprimorar o código do desafio anterior.
-Adicionamos um sistema de placar, onde a cada questão certa o usuário acumula 3 pontos 
-multiplicado pelo multiplicador atual. A cada 5 níveis, o multiplicador deve ser acrescido em 1.'''
-
-
 import os
 import random
 
