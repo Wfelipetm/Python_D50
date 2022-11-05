3b474f98140afe2a4a033b7efc704715d7f5ab8e
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:44:17 -0300

    Update 048.py

--- 048/048.py
+++ 048/048.py
@@ -1,14 +1,3 @@
-'''Vamos melhorar ainda mais nosso sistema da lanchonete?
-Partindo do código do desafio 47, vamos alterar para que os pedidos sejam escritos em um 
-arquivo "pedidos.txt" em vez de imprimir na tela quando o sistema finaliza.
-Gere um outro arquivo "relatorio.txt" contendo a quantidade de cada item vendido,
-o valor faturado por item e o valor faturado total.
-OBSERVAÇÃO: Faça append do arquivo em vez de sobrescrever ele. Assim, cada vez que 
-o sistema reinicia, ele irá adicionar os pedidos novos do próximo dia sem perder os do dia anterior.'''
-
-
-
-
 import os
 from datetime import datetime
 
