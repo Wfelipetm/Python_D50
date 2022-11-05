a0a55704821c2d9157b4ffe8667027666265c788
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:45:58 -0300

    Update 046.py

--- 046/046.py
+++ 046/046.py
@@ -1,10 +1,3 @@
-'''Agora leia um arquivo texto contendo várias frases e utilize a função gerada no desafio 45.
-Como resultado escreva outro arquivo de saída com o texto embaralhado.
-
-Tente ler o texto gerado como resultado.
-Vcoê vreá que o seu crbéreo é cpaaz de ler o ttxeo se memianvrtos a piremria e a úilmta lrtea iátntca.'''
-
-
 import random
 
 
