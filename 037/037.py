b2766a08089e9e81c62ff9096f2c239213291c08
Wallace Tavares <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 18:30:46 -0300

    Update 037.py

--- 037/037.py
+++ 037/037.py
@@ -1,6 +1,3 @@
-'''Vamos fazer um programa que converte texto em português para código morse.
-Leia um texto digitado pelo usuário e utilize um mapeamento para fazer a conversão do texto para morse.'''
-
 import dicionario_morse
 
 
