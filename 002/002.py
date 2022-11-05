28da49e7ee912e53b9bd174b056eba4818fa6e0a
Wfelipetm <108297008+Wfelipetm@users.noreply.github.com>
2022-11-05 17:29:02 -0300

    Create 002.py

---
+++ 002/002.py
@@ -0,0 +1,21 @@
+'''Faça um programa que leia 3 valores 
+inteiros e imprima o maior e o menor deles!'''
+
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+c = int(input('Digite o terceiro valor: '))
+
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+print(f'O menor valor foi {menor}')
+print(f'O maior valor foi {maior}')
